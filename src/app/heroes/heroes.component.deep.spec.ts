e238538cb913d4baa811e3086b70394178c38567
amyyan210 <amy@yanaway.com>
2018-08-28 14:16:55 -0500

    Deep integration test on HeroesComponent

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -1,53 +1,53 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { HeroesComponent } from './heroes.component';
-// import { NO_ERRORS_SCHEMA, Component, Input, Output } from '@angular/core';
-// import { HeroService } from '../hero.service';
-// import { of } from 'rxjs/observable/of';
-// import { EventEmitter } from 'selenium-webdriver';
-// import { By } from '@angular/platform-browser';
-// import { Hero } from '../hero';
-// import { HeroComponent } from '../hero/hero.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesComponent } from './heroes.component';
+import { NO_ERRORS_SCHEMA, Component, Input, Output } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { of } from 'rxjs/observable/of';
+import { EventEmitter } from 'selenium-webdriver';
+import { By } from '@angular/platform-browser';
+import { Hero } from '../hero';
+import { HeroComponent } from '../hero/hero.component';
 
-// describe('HeroesComponent (deep tests)', () => {
-//   let fixture: ComponentFixture<HeroesComponent>;
-//   let mockHeroService;
-//   let HEROES;
+describe('HeroesComponent (deep tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
 
-//   beforeEach(() => {
-//     HEROES = [
-//       {id: 1, name: 'SpiderDude', strength: 8},
-//       {id: 2, name: 'Wonderful Woman', strength: 24},
-//       {id: 3, name: 'SuperDude', strength: 55}
-//     ]
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: 'SpiderDude', strength: 8},
+      {id: 2, name: 'Wonderful Woman', strength: 24},
+      {id: 3, name: 'SuperDude', strength: 55}
+    ];
 
-//     mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         HeroesComponent,
-//         HeroComponent],
-//       providers: [{
-//         provide: HeroService, useValue: mockHeroService
-//       }],
-//       schemas: [NO_ERRORS_SCHEMA]
-//     })
-//     fixture = TestBed.createComponent(HeroesComponent);
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        HeroComponent],
+      providers: [{
+        provide: HeroService, useValue: mockHeroService
+      }],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    fixture = TestBed.createComponent(HeroesComponent);
 
-//   });
+  });
 
-//   it('should render each hero as a HeroComponent', () => {
-//     mockHeroService.getHeroes.and.returnValue(of(HEROES));
+  it('should render each hero as a HeroComponent', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
 
-//     // run ngOnItInt
-//     fixture.detectChanges();
+    // run ngOnItInt
+    fixture.detectChanges();
 
-//     const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
-//     expect(heroComponentDEs.length).toBe(3);
+    const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    expect(heroComponentDEs.length).toEqual(3);
 
-//     for(let i = 0; i < heroComponentDEs.length; i++) {
-//       expect(heroComponentDEs[i].componentInstance.hero.name).toEqual(heroComponentDEs[i]);
+    for(let i = 0; i < heroComponentDEs.length; i++) {
+      expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
 
-//     }
-//   })
+    }
+  });
 
-// })
+})
